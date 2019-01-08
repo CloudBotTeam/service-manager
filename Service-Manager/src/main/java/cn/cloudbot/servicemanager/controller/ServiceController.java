dccd194b168d31fc3dd09e6268c6c99a7a2a1b99
ChenCyl <programming2@163.com>
2019-01-08 17:24:03 +0800

    modified:   CloudBot-Common 	modified:   Service-Manager/src/main/java/cn/cloudbot/servicemanager/controller/ServiceController.java

--- Service-Manager/src/main/java/cn/cloudbot/servicemanager/controller/ServiceController.java
+++ Service-Manager/src/main/java/cn/cloudbot/servicemanager/controller/ServiceController.java
@@ -3,13 +3,11 @@ package cn.cloudbot.servicemanager.controller;
 import cn.cloudbot.servicemanager.service.rss.pojo.ServicePOJO;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+@CrossOrigin
 @RestController
 public class ServiceController {
     @Autowired
