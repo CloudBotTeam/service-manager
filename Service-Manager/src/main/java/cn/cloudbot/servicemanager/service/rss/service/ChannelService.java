6d93f32373a086111541d4a556318ada62de79da
ChenCyl <programming2@163.com>
2019-05-31 11:07:23 +0800

    fix: ssr hub deployed on srszd.top

--- Service-Manager/src/main/java/cn/cloudbot/servicemanager/service/rss/service/ChannelService.java
+++ Service-Manager/src/main/java/cn/cloudbot/servicemanager/service/rss/service/ChannelService.java
@@ -17,7 +17,8 @@ public class ChannelService {
     @Autowired
     private RestTemplate restTemplate;
 
-    String route = "http://maplewish.cn:1200";
+//    String route = "http://maplewish.cn:1200";
+    String route = "http://srszd.top:1200";
 
     /**
      * 获取软件学院实时通知（新闻动态
